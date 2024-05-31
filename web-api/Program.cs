6178d30ead4171b3a0ccc4766d977a2a7600ba2b
Muhammed Talha Karaca <89966125+mtalhakrc@users.noreply.github.com>
2024-05-31 17:42:31 +0300

    bearer token

--- web-api/Program.cs
+++ web-api/Program.cs
@@ -7,22 +7,10 @@ using web_api.Models;
 using web_api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
-//Starting...
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddAuthentication();
-    // .AddBearerToken(IdentityConstants.BearerScheme,
-    // o =>
-    // {
-    //     o.BearerTokenExpiration = TimeSpan.FromDays(360);
-    // });
-
 builder.Services.AddScoped<IRandevuService, RandevuService>();
 builder.Services.AddIdentityApiEndpoints<IdentityUser>()
     .AddEntityFrameworkStores<AppDbContext>();
@@ -38,35 +26,24 @@ builder.Services.AddSwaggerGen(options =>
 
     options.OperationFilter<SecurityRequirementsOperationFilter>();
 });
-//Ending...
-var app = builder.Build();
 
+
+
+var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.MapIdentityApi<IdentityUser>();
 app.UseHttpsRedirection();
+app.MapIdentityApi<IdentityUser>();
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseRouting();
 app.UseAuthorization();
 app.MapControllers();
-app.UseHttpsRedirection();
-
-// app.MapGet("/weatherforecast", () =>
-// {
-//     var forecast = Enumerable.Range(1, 5).Select(index =>
-//             new WeatherForecast
-//             (
-//                 DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-//                 Random.Shared.Next(-20, 55),
-//                 summaries[Random.Shared.Next(summaries.Length)]
-//             ))
-//         .ToArray();
-//     return forecast;
-// });
-
 app.Run();
 
 
